a9e96eb2859e178a749c4351813366028f85106b
MuhammadTarekUllah <142986550+MuhammadTarekUllah@users.noreply.github.com>
2023-08-31 11:21:48 +0100

    Add files via upload

---
+++ Geeks for geeks C Program - Basic/Print_entered_integer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num;
+
+    // Input the integer
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+
+    printf("Entered integer is: %d", num);
+
+    return 0;
+}
+Output:
+
